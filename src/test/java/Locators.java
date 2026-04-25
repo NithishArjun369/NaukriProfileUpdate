3b4d9baa53d096c693fdfee7cf4bda0c05dd6ab9
Nithish <babunithish36@gmail.com>
2026-04-26 00:04:47 +0530

    Replace Update_resume_Button with File_Input locator

--- src/test/java/Locators.java
+++ src/test/java/Locators.java
@@ -8,7 +8,7 @@ public class Locators {
 	public static final By Password = By.xpath("//label[contains(text(),'Password')]/following::input[1]");
 	public static final By Login_continue_Button = By.xpath("//button[text()='Login']");
 	public static final By View_Profile = By.xpath("(//a[contains(text(),'View')])[1]");
-	public static final By Update_resume_Button = By.xpath("//input[@value='Update resume']");
+	public static final By File_Input = By.id("attachCV");
 	public static final By Last_Updated = By.cssSelector("span.mod-date-val");
 
 }
