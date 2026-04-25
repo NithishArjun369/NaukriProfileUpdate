3fbc7b36587aefdf324c4913743da5093a7b8604
Nithish <babunithish36@gmail.com>
2026-04-25 19:33:31 +0530

    Add proper Maven structure with src/test/java

---
+++ src/test/java/Locators.java
@@ -0,0 +1,14 @@
+
+import org.openqa.selenium.By;
+
+public class Locators {
+	
+	public static final By Login_Button = By.xpath("//a[text()='Login']");
+	public static final By Username = By.xpath("//label[contains(text(),'Username')]/following::input[1]");
+	public static final By Password = By.xpath("//label[contains(text(),'Password')]/following::input[1]");
+	public static final By Login_continue_Button = By.xpath("//button[text()='Login']");
+	public static final By View_Profile = By.xpath("(//a[contains(text(),'View')])[1]");
+	public static final By Update_resume_Button = By.xpath("//input[@value='Update resume']");
+	public static final By Last_Updated = By.cssSelector("span.mod-date-val");
+
+}
---
+++ src/test/java/Update_resume.java
@@ -0,0 +1,67 @@
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class Update_resume  {
+
+	@Test
+	public void updateResume() throws AWTException, InterruptedException, IOException {
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--disable-gpu");
+		WebDriver driver = new ChromeDriver(options);
+		
+		Properties properties = new Properties();
+		FileInputStream fis = new FileInputStream("src/test/resources/Config.properties");
+		properties.load(fis);
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
+		WebDriverWait Wait = new WebDriverWait(driver,Duration.ofSeconds(3000));
+		
+		driver.get(properties.getProperty("url"));
+		driver.manage().window().maximize();
+		driver.findElement(Locators.Login_Button).click();
+		driver.findElement(Locators.Username).sendKeys(properties.getProperty("Username"));
+		driver.findElement(Locators.Password).sendKeys(properties.getProperty("Password"));
+		driver.findElement(Locators.Login_continue_Button).click();
+		Wait.until(ExpectedConditions.elementToBeClickable(Locators.View_Profile)).click();
+		driver.findElement(Locators.Update_resume_Button).click();
+		Thread.sleep(2000);
+		
+		String resumePath = System.getProperty("user.dir") + "/Nithish_QA.pdf";
+		String autoITPath = System.getProperty("user.dir") + "/fileupload.exe";
+		ProcessBuilder pb = new ProcessBuilder(autoITPath, resumePath);
+		pb.start();
+		
+		String LastUpdated = driver.findElement(Locators.Last_Updated).getText();
+		Assert.assertEquals(LastUpdated, "Today");
+		System.out.println("Resume updated successfully");
+		driver.quit();
+	}
+
+}
